3cb892ed015ed1448f7abe2add79caf1901ba84b
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2017-05-12 12:14:51 -0700

    update from stack.ip to ipaddress module

--- src/command/stack/commands/report/dhcpd/ace/__init__.py
+++ src/command/stack/commands/report/dhcpd/ace/__init__.py
@@ -1,31 +1,31 @@
 # @SI_Copyright@
 #                             www.stacki.com
 #                                  v3.1
-# 
+#
 #      Copyright (c) 2006 - 2016 StackIQ Inc. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-#  
+#
 # 1. Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-#  
+#
 # 2. Redistributions in binary form must reproduce the above copyright
 # notice unmodified and in its entirety, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided 
+# following disclaimer in the documentation and/or other materials provided
 # with the distribution.
-#  
+#
 # 3. All advertising and press materials, printed or electronic, mentioning
-# features or use of this software must display the following acknowledgement: 
-# 
-# 	 "This product includes software developed by StackIQ" 
-#  
+# features or use of this software must display the following acknowledgement:
+#
+# 	 "This product includes software developed by StackIQ"
+#
 # 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
 # neither the name or logo of this software nor the names of its
 # authors may be used to endorse or promote products derived from this
 # software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY STACKIQ AND CONTRIBUTORS ``AS IS''
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -42,13 +42,13 @@
 
 import stack
 import stack.commands
-import stack.ip
+import ipaddress
 import stack.text
 
 header = """ddns-update-style none;
 
 option space pxepi;
-option pxepi.six	code 6		= unsigned integer 8; 
+option pxepi.six	code 6		= unsigned integer 8;
 option pxepi.ten	code 10		= string;
 option pxepi.nine	code 9		= string;
 option pxepi.end	code 255	= boolean;
@@ -93,10 +93,10 @@ class Command(stack.commands.HostArgumentProcessor,
                 servers = {}
                 for row in self.db.select("""
                        	s.name, n.ip from
-                	nodes nd, subnets s, networks n where 
-			s.id       = n.subnet and 
-			s.pxe      = TRUE     and 
-			n.node     = nd.id    and 
+                	nodes nd, subnets s, networks n where
+			s.id       = n.subnet and
+			s.pxe      = TRUE     and
+			n.node     = nd.id    and
         		nd.name    = '%s'
 			""" % self.db.getHostname()):
 
@@ -110,7 +110,7 @@ class Command(stack.commands.HostArgumentProcessor,
 		# subnet info
 		#
                 for (netname, network, netmask, gateway, zone) in self.db.select("""
-        		name, address, mask, gateway, zone from 
+        		name, address, mask, gateway, zone from
 			subnets where
         		pxe = TRUE
 		        """):
@@ -121,17 +121,18 @@ class Command(stack.commands.HostArgumentProcessor,
 			self.addOutput('', '\tdefault-lease-time\t\t1200;')
 			self.addOutput('', '\tmax-lease-time\t\t\t1200;')
 
-			ipg  = stack.ip.IPGenerator(network, netmask)
-                        self.addOutput('', '\toption routers\t\t\t%s;' % gateway)
+			ipnetwork  = ipaddress.IPv4Network(unicode(
+				network + '/' + netmask))
+            self.addOutput('', '\toption routers\t\t\t%s;' % gateway)
 			self.addOutput('', '\toption subnet-mask\t\t%s;' % netmask)
 			self.addOutput('', '\toption broadcast-address\t%s;' %
-                        	ipg.broadcast())
+                        	ipnetwork.broadcast_address)
 			self.addOutput('', '}\n')
 
                 data = {}
 		for row in self.db.select("name from nodes order by rack, rank"):
                         data[row[0]] = []
-                        
+
 		for row in self.db.select("""
 			nodes.name, n.mac, n.ip, n.device
 			from networks n, nodes where
@@ -145,13 +146,13 @@ class Command(stack.commands.HostArgumentProcessor,
                 for name in data.keys():
                         kickstartable = self.str2bool(
                                 self.getHostAttr(name, 'kickstartable'))
-			
+
 			appliance = self.getHostAttr(name, "appliance")
-                        
+
                        	mac = None
                        	ip  = None
                        	dev = None
-                        
+
 			#
 			# look for a physical private interface that has an
 			# IP address assigned to it.
@@ -183,8 +184,8 @@ class Command(stack.commands.HostArgumentProcessor,
                                                 server = servers.get(netname)
                                                 if not server:
                                                         server = servers.get('default')
-						
-						if appliance.startswith("ace") or appliance == "frontend": 
+
+						if appliance.startswith("ace") or appliance == "frontend":
 							self.addOutput('', '\toption root-path\t"%s:/export/nfs/install";' % server)
 
 							self.addOutput('',
@@ -194,7 +195,7 @@ class Command(stack.commands.HostArgumentProcessor,
 							self.addOutput('', '\tfilename\t\t"pxelinux.0";')
 							self.addOutput('', '\tserver-name\t\t"%s";' % server)
 							self.addOutput('', '\tnext-server\t\t%s;' % server)
-                                
+
                                         self.addOutput('', '}')
 
 		self.addOutput('', '</stack:file>')
